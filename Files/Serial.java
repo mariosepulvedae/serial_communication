edb905b2c422c11d2b398ac1b10d17610fff502f
mariosepulvedae <eduardo.sh3100@gmail.com>
2026-02-16 00:35:19 -0600

    Opening serialport

--- Files/Serial.java
+++ Files/Serial.java
@@ -1,6 +1,23 @@
+import java.io.IOException;
+import java.net.http.*;
+import com.fazecast.jSerialComm.SerialPort;
+import java.io.InputStream;
+
 public class Serial{
     public static void main(String[] args){
         System.out.println("Hellooou");
+
+        SerialPort serial = SerialPort.getCommPorts()[0];
+
+        InputStream comPortA ;
+        int readAscii;
+
+        try{
+
+        }catch(IOException ex){
+            System.out.println("Error opening the serial port: "+ex.getMessage());
+        }
+
 
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 ### Serial communication with java
 
+To run this project you just need to run this command
 ```
-javac Serial.java
+javac javac -cp ".:./jSerialComm-X.X.X.jar" Serial.java
 ```
+Then, you can run the whole application with the next command:
+```
+java -cp ".:./jSerialComm-X.X.X.jar" Serial
+```
+where ```X.X.X``` refers to the current version of the jSerialComm library.
+
+You can check the jSerialComm documentation [here](https://fazecast.github.io/jSerialComm/)
